5c41931efb2c340901f548afbebc4633216b6c22
vladimirnikitin01 <nikitin.vg@phystech.edu>
2020-10-15 13:11:15 +0300

    Update B.cpp
    ID 36432200

--- B.cpp
+++ B.cpp
@@ -1,37 +1,37 @@
 ﻿/*Строка называется палиндромом, если она одинаково читается как слева направо, так и справа налево. Например, «abba» – палиндром, а «roman» – нет.
 Для заданной строки s длины n (1 ≤ n ≤ 105) требуется подсчитать число пар (i, j), 1 ≤ i < j ≤ n, таких что подстрока s[i..j] является палиндромом.*/
 
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include<algorithm>
 
-using namespace std;
-
-vector<long long> algorithmManager(string& s) {
-	vector<long long> quantity_in_i(s.size(), 1);
-	int l = 0, r = -1;
-	for (long long i = 0; i < s.size(); ++i) {
-		long long k = 1;
-		if (i <= r) {
-			k = min(r - i + 1, quantity_in_i[l + r - i]);
+char separating_character = '#';
+std::vector<uint64_t> algorithmManager(const std::string& s) {
+	std::vector<uint64_t> quantity_in_i(s.size(), 1);
+	int64_t l = 0, r = -1;
+	for (uint64_t i = 0; i < s.size(); ++i) {
+		uint64_t k = 1;
+		if (r >= 0 && i <= r) {
+			k = std::min(r - i + 1, quantity_in_i[l + r - i]);
 		}
-		while (i + k < s.size() && i - k >= 0 && s[i + k] == s[i - k]) {
+		while (i + k < s.size() && i >= k && s[i + k] == s[i - k]) {
 			++k;
 		}
 		quantity_in_i[i] = k;
-		if (k + i - 1 > r) {
-			l = i - k + 1;
+		if (k + i > r + 1) {
+			l = 1 + i - k;
 			r = k + i - 1;
 		}
 	}
 	return quantity_in_i;
 }
 
-long long numberOfPalindromes(string& now, vector<long long>& quantity_array) {
-	long long sum = 0;
-	for (unsigned int i = 0; i < now.size(); ++i) {
-		if (now[i] == '#') {
+uint64_t numberOfPalindromes(const std::string& now, const std::vector<uint64_t>& quantity_array) {
+	int64_t sum = 0;
+	for (uint64_t i = 0; i < now.size(); ++i) {
+		if (now[i] == separating_character) {
 			sum += quantity_array[i] / 2;
 		}
 		else {
@@ -41,19 +41,21 @@ long long numberOfPalindromes(string& now, vector<long long>& quantity_array) {
 	return sum;
 }
 
-long long algorithm(string& s) {
-	string new_s = "#";
-	for (unsigned int i = 0; i < s.size(); ++i) {
+uint64_t algorithm(const std::string& s) {
+	std::string new_s;
+	new_s.reserve(2 * s.size());
+	new_s += separating_character;
+	for (uint64_t i = 0; i < s.size(); ++i) {
 		new_s += s[i];
-		new_s += "#";
+		new_s += separating_character;
 	}
-	vector<long long> quantity_array = algorithmManager(new_s);
+	std::vector<uint64_t> quantity_array = algorithmManager(new_s);
 	return (numberOfPalindromes(new_s, quantity_array));
 }
 
 
 int main() {
-	string s;
-	cin >> s;
-	cout << algorithm(s);
+	std::string s;
+	std::cin >> s;
+	std::cout << algorithm(s);
 }
