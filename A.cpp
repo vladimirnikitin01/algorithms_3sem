e23ad74c3d47b6d1c1bcbb0772e50f26d9fbadc6
vladimirnikitin01 <nikitin.vg@phystech.edu>
2020-10-15 13:00:38 +0300

    Update A.cpp
    ID 36431211

--- A.cpp
+++ A.cpp
@@ -2,28 +2,33 @@
 p ≤ 30000, n ≤ 300000.
 Вариант 2. С помощью z-функции.*/
 
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
 
-using namespace std;
-
 struct array_z_and_l_r {
-	vector<int> z;
-	int l = 0, r = 0;
-	array_z_and_l_r(vector<int> array, int L, int R) {
-		z = array;
+	std::vector<uint64_t> z;
+	uint64_t l = 0, r = 0;
+	array_z_and_l_r() {}
+	array_z_and_l_r(std::vector<uint64_t> array, uint64_t L, uint64_t R) {
+		z = std::move(array);
 		l = L;
 		r = R;
 	}
 };
 
-array_z_and_l_r z_function(string& s) {
-	vector<int> z(s.size(), 0);
-	int left = 0, right = 0;
-	for (int i = 1; i < s.size(); ++i) {
-		z[i] = max(min(z[i - left], right - i + 1), 0);
+array_z_and_l_r z_function(const std::string& s) {
+	std::vector<uint64_t> z(s.size(), 0);
+	uint64_t left = 0, right = 0;
+	for (uint64_t i = 1; i < s.size(); ++i) {
+		if (right < i - 1) {
+			z[i] = 0;
+		}
+		else {
+			z[i] = std::min(z[i - left], right - i + 1);
+		}
 		while (i + z[i] < s.size() && s[i + z[i]] == s[z[i]]) {
 			++z[i];
 		}
@@ -33,15 +38,15 @@ array_z_and_l_r z_function(string& s) {
 		}
 	}
 
-	return (array_z_and_l_r(z, left, right));
+	return { z, left, right };
 }
 
-void z_function_main(string& s, string& pattern, array_z_and_l_r information_pattern) {
-	int left = information_pattern.l, right = information_pattern.r;
-	for (int i = 1 + information_pattern.z.size(); i < s.size() + information_pattern.z.size() + 1; ++i) {
-		int z_now = 0;
-		if (right - i + 1 > 0) {
-			z_now = (min(information_pattern.z[i - left], right - i + 1));
+void z_function_main(const std::string& s, const  std::string& pattern, const array_z_and_l_r information_pattern) {
+	uint64_t left = information_pattern.l, right = information_pattern.r;
+	for (uint64_t i = 1 + information_pattern.z.size(); i < s.size() + information_pattern.z.size() + 1; ++i) {
+		uint64_t z_now = 0;
+		if (right > i - 1) {
+			z_now = (std::min(information_pattern.z[i - left], right - i + 1));
 		}
 		while (i + z_now < s.size() + information_pattern.z.size() + 1 && s[i + z_now - information_pattern.z.size() - 1] == pattern[z_now]) {
 			++z_now;
@@ -51,20 +56,20 @@ void z_function_main(string& s, string& pattern, array_z_and_l_r information_pat
 			right = z_now + i - 1;
 		}
 		if (z_now == information_pattern.z.size()) {
-			cout << i - 1 - information_pattern.z.size() << " ";
+			std::cout << i - 1 - information_pattern.z.size() << " ";
 		}
 	}
 }
 
-void algorithm(string& pattern, string& text) {
+void algorithm(const std::string& pattern, const std::string& text) {
 	auto z = z_function(pattern);
 	z_function_main(text, pattern, z);
 }
 
 int main()
 {
-	string pattern;
-	string text;
-	cin >> pattern >> text;
+	std::string pattern;
+	std::string text;
+	std::cin >> pattern >> text;
 	algorithm(pattern, text);
 }
